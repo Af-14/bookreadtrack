8755504477f8216ade93a938260519e7f6e526f7
Af-14 <108504483+Af-14@users.noreply.github.com>
2022-10-28 17:55:42 +0800

    Update MyList.js

--- src/Components/MyList.js
+++ src/Components/MyList.js
@@ -1,72 +1,72 @@
 import React from "react";
 import MyListDetail from "./MyListDetail/MyListDetail";
-import BookCrud from "./MyListDetail/BookCRUD/BookCrud";
+// import BookCrud from "./MyListDetail/BookCRUD/BookCrud";
 
-const img_arr = [
-  {
-    id: 1,
-    src: "./img/1.jpeg",
-    title: "Fortress of Blood",
-    description: "L.D. Goffigan",
-  },
-  {
-    id: 2,
-    src: "./img/2.jpg",
-    title: "The king of Drugs",
-    description: "Nora Barret",
-  },
-  {
-    id: 3,
-    src: "./img/3.jpeg",
-    title: "Crake the Code",
-    description: "Patrick C.Harless",
-  },
-  {
-    id: 4,
-    src: "./img/4.jpg",
-    title: "Kissing in the Crow's Nest",
-    description: "Ariel Dalziel",
-  },
-];
+// const img_arr = [
+//   {
+//     id: 1,
+//     src: "./img/1.jpeg",
+//     title: "Fortress of Blood",
+//     description: "L.D. Goffigan",
+//   },
+//   {
+//     id: 2,
+//     src: "./img/2.jpg",
+//     title: "The king of Drugs",
+//     description: "Nora Barret",
+//   },
+//   {
+//     id: 3,
+//     src: "./img/3.jpeg",
+//     title: "Crake the Code",
+//     description: "Patrick C.Harless",
+//   },
+//   {
+//     id: 4,
+//     src: "./img/4.jpg",
+//     title: "Kissing in the Crow's Nest",
+//     description: "Ariel Dalziel",
+//   },
+// ];
 
-const img_arr3 = [
-  {
-    id: 1,
-    src: "./img/11.jpg",
-    title: "Water & Flame",
-    description: "Alejandra Vega P.E Padilla",
-  },
-  {
-    id: 2,
-    src: "./img/12.jpg",
-    title: "The Way of the Nameless",
-    description: "Graham Douglasss",
-  },
-  {
-    id: 3,
-    src: "./img/13.jpg",
-    title: "A Journey to the Moon",
-    description: "Max Born",
-  },
-  {
-    id: 4,
-    src: "./img/14.jpg",
-    title: "Night Time Stories",
-    description: "Ken Adams",
-  },
-  {
-    id: 5,
-    src: "./img/15.jpg",
-    title: "Lost in the Woods",
-    description: "Marvin Navina",
-  },
-  {
-    id: 6,
-    src: "./img/16.jpg",
-    title: "The Gondola Maker",
-    description: "Laura Morelli",
-  },
-];
+// const img_arr3 = [
+//   {
+//     id: 1,
+//     src: "./img/11.jpg",
+//     title: "Water & Flame",
+//     description: "Alejandra Vega P.E Padilla",
+//   },
+//   {
+//     id: 2,
+//     src: "./img/12.jpg",
+//     title: "The Way of the Nameless",
+//     description: "Graham Douglasss",
+//   },
+//   {
+//     id: 3,
+//     src: "./img/13.jpg",
+//     title: "A Journey to the Moon",
+//     description: "Max Born",
+//   },
+//   {
+//     id: 4,
+//     src: "./img/14.jpg",
+//     title: "Night Time Stories",
+//     description: "Ken Adams",
+//   },
+//   {
+//     id: 5,
+//     src: "./img/15.jpg",
+//     title: "Lost in the Woods",
+//     description: "Marvin Navina",
+//   },
+//   {
+//     id: 6,
+//     src: "./img/16.jpg",
+//     title: "The Gondola Maker",
+//     description: "Laura Morelli",
+//   },
+// ];
 
 export default class MyList extends React.Component {
   render() {
